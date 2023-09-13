7bdcfb1b209a744c7af7493d521f67135c1be738
giuseppevignanello <peppe.vignanello@gmail.com>
2023-09-13 15:34:40 +0200

    add variables

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -1,5 +1,11 @@
 package org.lessons.java.security;
 
 public class PasswordGenerator {
-
+	public static void main(String[] args) {
+		String name = null; 
+		String surname = null; 
+		String favoriteColor = null; 
+		int[] birthData = new int[3];
+		
+	}
 }
