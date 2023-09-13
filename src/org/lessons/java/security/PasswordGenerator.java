1869b9d80affcb7e6c07e54f7647026c0fe5b003
giuseppevignanello <peppe.vignanello@gmail.com>
2023-09-13 15:42:00 +0200

    Ask user info

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -1,11 +1,35 @@
 package org.lessons.java.security;
 
+import java.util.Scanner;
+
 public class PasswordGenerator {
 	public static void main(String[] args) {
-		String name = null; 
-		String surname = null; 
-		String favoriteColor = null; 
-		int[] birthData = new int[3];
+	
+		int[] birthDate = new int[3];
+		
+		Scanner sc = new Scanner(System.in); 
+		//ask the name
+		System.out.println("Your name is: ");
+		String name = sc.nextLine(); 
+		
+		//ask the surname
+		System.out.println("Your surname is: ");
+		String surname = sc.nextLine();
+		
+		//ask the favorite color
+		System.out.println("Your name is: ");
+		String favoriteColor = sc.nextLine(); 
+		
+		//ask the birth day 
+		System.out.println("Your birth year is: ");
+		 birthDate[2] = sc.nextInt(); 
+		 
+		 System.out.println("Your birth month is (number): ");
+		 birthDate[1] = sc.nextInt(); 
+		 
+		 System.out.println("Your birth day is: ");
+		 birthDate[0] = sc.nextInt(); 
+		
 		
 	}
 }
